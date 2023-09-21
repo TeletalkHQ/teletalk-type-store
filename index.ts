3ea6379dbfe274ea68bf526a4caf5ba429e63ced
S-STALWART-S <stalwart.programmer@gmail.com>
2023-09-21 08:28:30 +0330

    fix

---
+++ index.ts
@@ -0,0 +1,10 @@
+import { countries } from "./src/variables/countries";
+
+export interface StringMap {
+  [key: string]: any;
+}
+
+export * from "./src/datatypes";
+export * from "./src/api";
+
+export { countries };
